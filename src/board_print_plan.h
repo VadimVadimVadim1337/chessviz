35d630e8e04db04a07e2e74bd0547a01467e2600
VadimVadimVadim1337 <vadim_vadim_k@mail.ru>
2018-04-17 16:30:23 +0700

    The code is moved to another source because I was mistaken where I wrote

---
+++ src/board_print_plan.h
@@ -0,0 +1,26 @@
+#include "Board_print_html.h"
+   void printBoard(){
+    	int i_i=0;
+    	int numeral_board=8;
+    	for (int r = 0; r < 8; r++){
+    		for (int c = 0; c < 8; c++){
+                
+                if (i_i==0 || i_i==8 || i_i==16 || i_i==24 || i_i==32
+    				|| i_i==40 || i_i==48 || i_i==56){
+    			printf("%d", numeral_board);
+    	          numeral_board--;}  //для нумерации по столбцу
+
+                //colorBoard(c,r);
+                i_i++;
+    			printf("[");
+    			zero_color;
+    			printf("%c", *(board[r][c]));
+                //colorBoard(c,r);
+    			printf("]");
+    			zero_color;
+ 
+    		}
+    		printf("\n");
+    	}
+    	printf("  h  g  f  e  d  c  b  a\n"); //нумерация по вертикали
+    }
